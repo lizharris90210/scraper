a7dbf732f1be68dc6f914f076c0a0b5483ea282b
Liz Harris <lizharris90210@gmail.com>
2019-12-15 22:25:30 -0500

    works on localhost

--- public/app.js
+++ public/app.js
@@ -3,7 +3,7 @@ $.getJSON("/articles", function (data) {
   // For each one
   for (var i = 0; i < data.length; i++) {
     // Display the apropos information on the page
-    $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p>");
+    $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + '<a href="http://apnews.com/"' + data[i].link + "> " + data[i].link + "</p></a>");
   }
 });
 
--- server.js
+++ server.js
@@ -28,24 +28,20 @@ app.use(express.static("public"));
 
 // Connect to the Mongo DB
 // // Deployed
-var MONGODB_URI = 
-// process.env.MONGODB_URI ;
-// ||
-// Local
-"mongodb://localhost/mongoHeadlines";
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/apheadlines";
 
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+mongoose.connect(MONGODB_URI, { useUnifiedTopology: true, useNewUrlParser: true });
 
 // Routes
 // A GET route for scraping the echoJS website
 app.get("/scrape", function (req, res) {
   // First, we grab the body of the html with axios
-  axios.get("http://www.echojs.com/").then(function (response) {
+  axios.get("https://apnews.com/").then(function (response) {
     // Then, we load that into cheerio and save it to $ for a shorthand selector
     var $ = cheerio.load(response.data);
 
     // Now, we grab every h2 within an article tag, and do the following:
-    $("article h2").each(function (i, element) {
+    $("div.CardHeadline").each(function (i, element) {
       // Save an empty result object
       var result = {};
 
@@ -108,7 +104,7 @@ app.post("/articles/:id", function (req, res) {
     });
 
 });
-  // Start the server
-  app.listen(PORT, function () {
-    console.log("App running on port " + PORT + "!");
-  });
+// Start the server
+app.listen(PORT, function () {
+  console.log("App running on port " + PORT + "!");
+});
